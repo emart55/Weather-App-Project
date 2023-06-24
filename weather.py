e93b824fd9c6fb1057cf01cb68e46d860d581592
Emily Martin <emilymartin@Emilys-MacBook-Pro.local>
2023-06-24 19:24:58 +1000

    first changes

--- weather.py
+++ weather.py
@@ -24,7 +24,15 @@ def convert_date(iso_string):
     Returns:
         A date formatted like: Weekday Date Month Year e.g. Tuesday 06 July 2021
     """
-    pass
+    from datetime import datetime
+
+    # Parse the ISO date into a datetime object
+    parsed_date = datetime.fromisoformat(iso_string)
+
+    # Format the datetime object into the desired format
+    formatted_date = parsed_date.strftime('%A %d %B %Y')
+
+    return formatted_date
 
 
 def convert_f_to_c(temp_in_farenheit):
@@ -35,8 +43,11 @@ def convert_f_to_c(temp_in_farenheit):
     Returns:
         A float representing a temperature in degrees celcius, rounded to 1dp.
     """
-    pass
-
+    int_temp = float(temp_in_farenheit)
+    celsius = (int_temp - 32) * 5 / 9
+    celsius_rounded = round(celsius, 1)
+    
+    return celsius_rounded
 
 def calculate_mean(weather_data):
     """Calculates the mean value from a list of numbers.
@@ -46,7 +57,13 @@ def calculate_mean(weather_data):
     Returns:
         A float representing the mean value.
     """
-    pass
+    numeric_numbers = [float(num) for num in weather_data]  # Convert strings to float
+    total = sum(numeric_numbers)
+    count = len(numeric_numbers)
+    average = total/count
+
+    return average
+
 
 
 def load_data_from_csv(csv_file):
@@ -57,7 +74,30 @@ def load_data_from_csv(csv_file):
     Returns:
         A list of lists, where each sublist is a (non-empty) line in the csv file.
     """
-    pass
+    import csv
+
+    with open(csv_file, 'r') as file:
+        rows = []  # List to store the rows
+        skip_header = True  # Flag to skip the first row
+        csv_reader = csv.reader(file)
+        for row in csv_reader:
+            if skip_header:
+                skip_header = False
+                continue  # Skip the first row
+            if all(not cell.strip() for cell in row):
+                continue
+            # Convert appropriate numeric values to integers
+            converted_row = []
+            for cell in row:
+                cell = cell.strip()
+                try:
+                    converted_row.append(int(cell))
+                except ValueError:
+                    converted_row.append(cell)
+            rows.append(converted_row)
+
+        return rows
+
 
 
 def find_min(weather_data):
@@ -68,7 +108,20 @@ def find_min(weather_data):
     Returns:
         The minium value and it's position in the list.
     """
-    pass
+    if not weather_data:
+        return () 
+
+    data = data = [float(item) for item in weather_data]
+
+    min_value = min(data)
+    min_index = data.index(min_value)
+
+    # Check if there is a second occurrence
+    if data.count(min_value) > 1:
+        # Find the index of the second occurrence
+        min_index = data.index(min_value, min_index + 1)
+
+    return min_value, min_index
 
 
 def find_max(weather_data):
@@ -79,7 +132,20 @@ def find_max(weather_data):
     Returns:
         The maximum value and it's position in the list.
     """
-    pass
+    if not weather_data:
+        return () 
+
+    data = data = [float(item) for item in weather_data]
+
+    max_value = max(data)
+    max_index = data.index(max_value)
+
+    # Check if there is a second occurrence
+    if data.count(max_value) > 1:
+        # Find the index of the second occurrence
+        max_index = data.index(max_value, max_index + 1)
+            
+    return max_value, max_index
 
 
 def generate_summary(weather_data):
@@ -90,7 +156,10 @@ def generate_summary(weather_data):
     Returns:
         A string containing the summary information.
     """
-    pass
+
+
+
+
 
 
 def generate_daily_summary(weather_data):
@@ -101,4 +170,27 @@ def generate_daily_summary(weather_data):
     Returns:
         A string containing the summary information.
     """
-    pass
+    summary = ""  # Initialize an empty string to accumulate the summary information
+
+    for day in weather_data:
+        date_str = day[0]
+        datetime_object = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S%z")
+        day_of_week = datetime_object.strftime("%A %d %B %Y")
+
+        min_temp = convert_f_to_c2(day[1])
+        max_temp = convert_f_to_c2(day[2])
+
+        formatted_date = "---- {} ----".format(day_of_week)
+        formatted_min_temp = "  Minimum Temperature: {:.1f}°C".format(min_temp)
+        formatted_max_temp = "  Maximum Temperature: {:.1f}°C".format(max_temp)
+
+        formatted_output = "\n".join([formatted_date, formatted_min_temp, formatted_max_temp])
+        summary += formatted_output + "\n\n"  # Append the formatted weather information to the summary string
+
+    return summary
+
+
+
+def convert_f_to_c2(temperature_f):
+    return (temperature_f - 32) * 5 / 9
+
